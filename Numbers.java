dbfba5778fe191a758727fcfa83e52bbca5ea33d
karthikkola26 <116869218+karthikkola26@users.noreply.github.com>
2022-10-28 12:41:04 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,66 @@
+public class Numbers
+{
+    public static void main(String args[]) 
+    {
+        int n=4;
+        for(int i=0;i<=n;i++)
+		{
+			for(int j=0 ; j<n; j++)
+			{
+				if(i==0 )
+				{
+					System.out.print("1");
+				}
+				else
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}
+            System.out.println(" ");
+            for(int j=0 ; j<n; j++)
+			{
+				if(i==0 )
+				{
+					System.out.print("2");
+				}
+				else
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}
+            System.out.println(" ");
+            for(int j=0 ; j<n; j++)
+			{
+				if(i==0 )
+				{
+					System.out.print("3");
+				}
+				else
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}
+            System.out.println(" ");
+            for(int j=0 ; j<n; j++)
+			{
+				if(i==0 )
+				{
+					System.out.print("4");
+				}
+				else
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}
+            System.out.println(" ");
+        }
+    }
+}
